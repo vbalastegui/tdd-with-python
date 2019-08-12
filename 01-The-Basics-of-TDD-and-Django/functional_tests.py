e9351dbfaf6c889314d343804602d98ec6525155
Victor Balastegui <victorbalastegui@gmail.com>
2019-08-12 16:40:25 +0200

    Front page HTML now generated from a template

--- 01-The-Basics-of-TDD-and-Django/functional_tests.py
+++ 01-The-Basics-of-TDD-and-Django/functional_tests.py
@@ -26,7 +26,7 @@ class NewVisitorTest(unittest.TestCase):
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
-            'enter a to-do item'
+            'Enter a to-do item'
         )
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
@@ -40,7 +40,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item. She
