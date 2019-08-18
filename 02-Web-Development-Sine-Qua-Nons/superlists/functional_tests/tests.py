4ebf8e25bb122d955e759d6edebfa230a6732d4b
Victor Balastegui <victorbalastegui@gmail.com>
2019-08-18 15:08:02 +0200

    Refactor templates to use a base template

--- 02-Web-Development-Sine-Qua-Nons/superlists/functional_tests/tests.py
+++ 02-Web-Development-Sine-Qua-Nons/superlists/functional_tests/tests.py
@@ -93,8 +93,30 @@ class NewVisitorTest(LiveServerTestCase):
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated an unique URL for her -- there is some
         # explanatory text to that effect.
-        self.fail('Finish the test!')
 
         # She visits that URL - her to-do list is still there.
 
         # Satisfied, she goes back to sleep
+
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
+
+        # She starts a new list and sees the input is nicely
+        # centered there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=5
+        )
